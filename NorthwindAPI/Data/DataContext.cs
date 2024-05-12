41b89f1a11d4da50ff7a0038ab1fd4dae33ddc45
avc285 <avc285@gmail.com>
2024-05-12 19:12:42 +0100

    Create data context

---
+++ NorthwindAPI/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindAPI.Models;
+
+namespace NorthwindAPI.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options):base(options)
+        {
+            
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Shipper> Shippers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // not sure whether this section is necessary, given that I have no m2m relationships.
+        }
+    }
+}
