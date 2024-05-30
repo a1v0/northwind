3982804df80501f26d61a044897bed74b4153897
a1v0 <avc285@gmail.com>
2024-05-30 21:26:03 +0100

    Rename and re-type Id field

--- NorthwindAPI/Models/Customer.cs
+++ NorthwindAPI/Models/Customer.cs
@@ -6,11 +6,11 @@ namespace NorthwindAPI.Models
 {
     public class Customer
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Company { get; set; }
-        public string City{ get; set; }
-        public string Country{ get; set; }
+        public string CustomerId { get; set; }
+        public string ContactName { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
         public ICollection<Order> Orders { get; set; }
     }
 }
