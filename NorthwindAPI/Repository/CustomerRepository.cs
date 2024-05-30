ccfa99b86be41c9c6e4351ddf14c7135e3d842a2
a1v0 <avc285@gmail.com>
2024-05-30 20:41:09 +0100

    Create GetCustomers() method

--- NorthwindAPI/Repository/CustomerRepository.cs
+++ NorthwindAPI/Repository/CustomerRepository.cs
@@ -1,7 +1,20 @@
+using NorthwindAPI.Data;
+using NorthwindAPI.Models;
+
 namespace NorthwindAPI.Repository
 {
     public class CustomerRepository
     {
+        public CustomerRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        private readonly DataContext _context;
         
+        public ICollection<Customer> GetCustomers()
+        {
+            return _context.Customer.OrderBy(p => p.Id).ToList();
+        }
     }
 }
