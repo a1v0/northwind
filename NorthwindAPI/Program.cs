e7218360baa261912daeb201cff83794132e5e4d
a1v0 <avc285@gmail.com>
2024-05-30 20:56:40 +0100

    Insert controller into Program file

--- NorthwindAPI/Program.cs
+++ NorthwindAPI/Program.cs
@@ -12,6 +12,7 @@ namespace NorthwindAPI
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
