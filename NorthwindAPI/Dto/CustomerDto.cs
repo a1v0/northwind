65ca491e75046373553f1cd5ae483a603b41d2a6
a1v0 <avc285@gmail.com>
2024-06-02 21:35:47 +0100

    Create DTO for Customer

---
+++ NorthwindAPI/Dto/CustomerDto.cs
@@ -0,0 +1,11 @@
+namespace NorthwindAPI.Dto
+{
+    public class CustomerDto
+    {
+        public string CustomerId { get; set; }
+        public string ContactName { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
