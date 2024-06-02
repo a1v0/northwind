22d2b80d1b0e44fb56ababd90a4e298d2582a5ab
a1v0 <avc285@gmail.com>
2024-06-02 21:15:22 +0100

    Add methods to interface

--- NorthwindAPI/Interfaces/ICustomerRepository.cs
+++ NorthwindAPI/Interfaces/ICustomerRepository.cs
@@ -5,5 +5,8 @@ namespace NorthwindAPI.Interfaces
     public interface ICustomerRepository
     {
         ICollection<Customer> GetCustomers();
+        Customer GetCustomer(string id);
+        bool CustomerExists(string id);
+        // add any other relevant methods relevant to the fields in the Customer table
     }
 }
