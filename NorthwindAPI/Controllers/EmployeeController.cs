f54198de085960d18c3c7192f38cdc2e93b5af25
a1v0 <avc285@gmail.com>
2024-06-02 18:30:47 +0100

    Create Employee controller

---
+++ NorthwindAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using NorthwindAPI.Interfaces;
+using NorthwindAPI.Models;
+
+namespace NorthwindAPI.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class EmployeeController : Controller
+    {
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        private readonly IEmployeeRepository _employeeRepository;
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Employee>))]
+
+        public IActionResult GetEmployees()
+        {
+            var employees = _employeeRepository.GetEmployees();
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(employees);
+        }
+    }
+}
