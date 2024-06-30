09f055fa01c1a902f64f266f09c8a4b71df04883
avc285 <avc285@gmail.com>
2024-06-30 22:06:24 +0100

    Add methods to EmployeeRepository

--- NorthwindAPI/Repository/EmployeeRepository.cs
+++ NorthwindAPI/Repository/EmployeeRepository.cs
@@ -15,7 +15,17 @@ namespace NorthwindAPI.Repository
 
         public ICollection<Employee> GetEmployees()
         {
-            return _context.Employees.OrderBy(p => p.EmployeeId).ToList();
+            return _context.Employees.OrderBy(p => p.Id).ToList();
+        }
+
+        public Employee GetEmployee(string id)
+        {
+            return _context.Employees.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public bool EmployeeExists(string id)
+        {
+            return _context.Employees.Any(p => p.Id == id);
         }
     }
 }
