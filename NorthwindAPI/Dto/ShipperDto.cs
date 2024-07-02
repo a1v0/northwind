4a3dbb92f575a89ede34f6cc9ea693ba76451176
a1v0 <avc285@gmail.com>
2024-07-02 20:09:13 +0100

    Repair inaccurate property name

--- NorthwindAPI/Dto/ShipperDto.cs
+++ NorthwindAPI/Dto/ShipperDto.cs
@@ -2,7 +2,7 @@ namespace NorthwindAPI.Dto
 {
     public class ShipperDto
     {
-        public string ShipperId { get; set; }
+        public int Id { get; set; }
         public string CompanyName { get; set; }
         public string Phone { get; set; }
     }
